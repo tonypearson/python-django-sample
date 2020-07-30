d5427247047e598d2a9d0c3a8fd29d4a7c42eabd
Github pipeline <pipeline@github.com>
2020-07-29 23:42:27 -0500

    Updates

--- app.sh
+++ app.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn --env DJANGO_SETTINGS_MODULE=pydjango.settings.prod pydjango.wsgi -b 0.0.0.0:8080
+gunicorn --env DJANGO_SETTINGS_MODULE=learning_logs.settings ./wsgi -b 0.0.0.0:3000
